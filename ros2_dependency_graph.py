524ed3f96f0554840f7d3ab8267eead6caa440eb
Joni Pöllänen <jonipol@users.noreply.github.com>
2023-07-29 14:10:53 +0300

    Use pyViz for visualization

--- ros2_dependency_graph.py
+++ ros2_dependency_graph.py
@@ -1,14 +1,16 @@
 import sys
 import xml.etree.ElementTree as ET
 import networkx as nx
-import matplotlib.pyplot as plt
 from pathlib import Path
-# from networkx.drawing.nx_pydot import write_dot
+from pyvis.network import Network
+import json
 
-
+# TODO: setup.py
 def main():
     graph = nx.DiGraph()
-    directory = str(sys.argv[0])
+    # TODO: Check for path validity
+    # TODO: Handle arg better
+    directory = str(sys.argv[1])
     package_xmls = [xml_path for xml_path in Path(directory).rglob('**/package.xml')]
 
     # Parse xml and add to graph
@@ -22,12 +24,59 @@ def main():
                 graph.add_node(child.text)
                 graph.add_edge(pkg.text, child.text)
 
-    nx.draw(graph, with_labels=True, font_weight="bold")
-    # pos = nx.nx_agraph.graphviz_layout(graph)
-    # pos = nx.spring_layout(graph)
-    # nx.draw(graph, pos=pos, with_labels=True)
-    # write_dot(graph, 'file.dot')
-    plt.show()
+    net = Network()
+    net.from_nx(graph)
+    net.show_buttons(filter_=True)
+    options = {
+        "configure": {
+            "enabled": True
+        },
+        "interaction": {
+            "hover": True
+        },
+        "nodes": {
+            "borderWidth": 2,
+            "borderWidthSelected": 6,
+            "chosen": True,
+            "color": {
+                "highlight": {
+                    "border": "#FF4040",
+                    "background": "#EE3B3B"
+                },
+                "hover": {
+                    "border": "#DEB887",
+                    "background": "#FFD39B"
+                }
+            }
+        },
+        "edges": {
+            "arrows": {
+                "to": True
+            },
+            "color": {
+                "color": "#00bfff",
+                "highlight": "#EE3B3B",
+                "hover": "#DEB887"
+            }
+        },
+        "layout": {
+            "hierarchical": {
+                "enabled": True,
+                "levelSeparation": 150,
+                "nodeSpacing": 400,
+                "treeSpacing": 150,
+                "blockShifting": True,
+                "edgeMinimization": True,
+                "parentCentralization": True,
+                "direction": "UD",
+                "sortMethod": "directed",
+                "shakeTowards": "leaves"
+            }
+        }
+    }
+    net.set_options(json.dumps(options))
+    # net.toggle_physics(False)
+    net.write_html("graph.html")
 
 
 if __name__ == "__main__":
